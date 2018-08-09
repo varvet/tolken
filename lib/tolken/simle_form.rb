cdf6e269509acde1065ccb7c6a2bbac5603c4818
Nicklas Ramhöj <nicklas.ramhoj@varvet.com>
2018-08-09 13:21:23 +0200

    Add simple_form jsonb input field

---
+++ lib/tolken/simle_form.rb
@@ -0,0 +1,33 @@
+require "rails"
+require "simple_form"
+require_relative "../tolken"
+
+module Tolken
+  module SimpleForm
+    class JsonbInput < ::SimpleForm::Inputs::StringInput
+      include ActionView::Helpers::OutputSafetyHelper
+
+      def input(_wrapper_options = {})
+        safe_join(
+          I18n.available_locales.map do |locale|
+            options = {
+              input_html: {
+                name: "#{object.class.to_s.downcase}[#{attribute_name}][#{locale}]",
+                value: object.public_send(attribute_name)[locale]
+              },
+              as: input_options[:type].presence || :string
+            }
+
+            @builder.input(:"#{attribute_name}_#{locale}", options)
+          end
+        )
+      end
+
+      def label(_wrapper_options = {})
+        # label is rendered per translatable field
+      end
+    end
+  end
+end
+
+JsonbInput = Tolken::SimpleForm::JsonbInput
--- tolken.gemspec
+++ tolken.gemspec
@@ -27,7 +27,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_dependency "activerecord", "5.2.0.rc2"
+  spec.add_dependency "rails", "5.2.0.rc2"
 
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
@@ -35,4 +35,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "pg"
   spec.add_development_dependency "with_model"
   spec.add_development_dependency "pry"
+  spec.add_development_dependency "simple_form"
 end
